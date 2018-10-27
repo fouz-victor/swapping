43128b8fdf938f95d9aaafe6196c8bd86dbf1888
fouz-victor <muhammedfouzankp@gmail.com>
2018-10-27 14:48:07 +0530

    Added logic

--- swap.c
+++ swap.c
@@ -5,7 +5,9 @@ void main()
     int y=5;
     printf("\n Values before swapping");
     printf("\n X: %d\n Y: %d\n",x,y);
-
+    int temp=x;
+    x=y;
+    y=temp;
     printf("\n Values after swapping");
     printf("\n X: %d\n Y: %d\n",x,y);
 }
